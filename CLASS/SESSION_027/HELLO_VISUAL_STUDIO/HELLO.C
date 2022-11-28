6326974d3d72b66aa38a8a4158d0b5a5d6be2f60
Sachn Jaiswal <sachin.jaiswal3@gmail.com>
2022-11-28 23:47:47 +0530

    MSTC initial programs

---
+++ CLASS/SESSION_027/HELLO_VISUAL_STUDIO/HELLO.C
@@ -0,0 +1,25 @@
+/*
+@AUTHOR: SACHIN JAISWAL
+@DATE: 27TH NOV 2022
+@GOAL: TO DISPLAY 'HELLO WORLD' ON CONSOL
+
+int printf(const char*, ...);
+void exit(int);
+
+int main(void)
+
+{
+	printf("Hello, Visual Studio\n");
+	exit(0);
+}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	printf("Hello Visual Studio\n");
+	exit(0);
+}
