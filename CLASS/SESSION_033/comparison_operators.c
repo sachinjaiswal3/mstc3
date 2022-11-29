be36b74a083ed0e41c517353a805432afc11694b
Sachn Jaiswal <sachin.jaiswal3@gmail.com>
2022-11-29 22:49:19 +0530

    SESSION_33

---
+++ CLASS/SESSION_033/comparison_operators.c
@@ -0,0 +1,35 @@
+/* 
+    @AUTHOR: Yogeshwar Shukla 
+    @DATE: 29th Nov 2022 
+    @GOAL: To demonstrate comparison operators 
+*/
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main(void)
+{
+    int n1 = 100; 
+    int n2 = 50; 
+    int rs; 
+
+    rs = (n1 > n2); 
+    printf("rs = %d\n", rs); 
+
+    rs = (n1 >= n2); 
+    printf("rs = %d\n", rs); 
+
+    rs = (n1 < n2); 
+    printf("rs = %d\n", rs); 
+
+    rs = (n1 <= n2); 
+    printf("rs = %d\n", rs); 
+
+    rs = (n1 == n2); 
+    printf("rs = %d\n", rs); 
+
+    rs = (n1 != n2); 
+    printf("rs = %d\n", rs); 
+
+    exit(0); 
+}
